12fa44a4bba6d0f50080fcc430aa298ee84d736e
Will Smith <will@willsmith.org>
2021-02-07 00:34:16 +0000

    decent unit tests for MakeFrameNumeric now

--- MakeFrameNumeric.py
+++ MakeFrameNumeric.py
@@ -3,40 +3,52 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
 
-class FrameTools:
+class MakeFrameNumeric:
 
     def __init__(self, df):
+         
+        if( not isinstance(df, pd.DataFrame)):
+            raise(TypeError,'df must be a DataFrame not a ' + str(type(df)))
+
+        self.__df = df
+        self.__maximum_cardinality_for_one_hot_encode = 3
     
-        self.df = df
-        self.maximum_cardinality_for_one_hot_encode = 3
-       
-    def listcols(self):
-        print(self.df.columns)
-
-
-    # xgboost needs the entire array to be numeric.
+    
+    
+    @property
+    def maximum_cardinality_for_one_hot_encode(self):
+        return self.__maximum_cardinality_for_one_hot_encode
+    
+    @maximum_cardinality_for_one_hot_encode.setter
+    def maximum_cardinality_for_one_hot_encode(self, newval):
+        self.__maximum_cardinality_for_one_hot_encode = newval
+        
+    
+    # xgboost needs the entire frame to be numeric.
     # So we'll convert strings to either one-hot or (if the cardinality is excessive for 1-hot) into a numerical label per unique string.
-    def convert_for_xgboost(self):
+    def ConvertForXGBoost(self):
     
         converted = pd.DataFrame()
     
         print('Converting...')
-        for col in self.df.columns:
+        for col in self.__df.columns:
             print('Column : ' + col)
-            print('Type: ' + str(type(self.df[col][0])))
-            print('Length: ' + str(len(self.df[col].unique())))
+            print('Type: ' + str(type(self.__df[col][0])))
+            
+            cardinality = len(self.__df[col].unique())
+            print('Cardinality: ' + str(cardinality))
             
             # If string, one-hot (if not too many unique values) or feature encode.
-            if isinstance(self.df[col][0], str):
+            if isinstance(self.__df[col][0], str):
                     
                     # Feature
                     label_encoder = LabelEncoder()
-                    feature = label_encoder.fit_transform(self.df[col])
-                    if len(self.df[col].unique()) > self.maximum_cardinality_for_one_hot_encode:
+                    feature = label_encoder.fit_transform(self.__df[col])
+                    if cardinality > self.__maximum_cardinality_for_one_hot_encode:
                         converted[col] = feature
                     else:
                         # One-hot
-                        feature = feature.reshape(self.df.shape[0], 1)
+                        feature = feature.reshape(self.__df.shape[0], 1)
                         onehot_encoder = OneHotEncoder(sparse=False, categories='auto')
                         feature = onehot_encoder.fit_transform(feature)
                         print(feature)
@@ -44,6 +56,6 @@ class FrameTools:
                             converted[col + '_' + str(f)] = feature[:,f]
             else:
                 # Already numeric, copy directly.
-                converted[col] = self.df[col]
+                converted[col] = self.__df[col]
             
-        print(converted)
+        return converted
--- TestMakeFrameNumeric.py
+++ TestMakeFrameNumeric.py
@@ -1,18 +1,79 @@
 import unittest
 import pandas as pd
-from TestMakeFrameNumeric import TestMakeFrameNumeric
+import numpy as np
+from MakeFrameNumeric import MakeFrameNumeric
 
 
 class TestMakeFrameNumeric(unittest.TestCase):
 
-    def testInitNotDataFrame(self):
+    def setUp(self):
+        data = {'id':   [1],
+                'value':[3]}                
+        self.validDataFrame = pd.DataFrame(data)
+        
+        
+    
+    def testInitBadParams1(self):
+        # Test bad calls to the constructor.
         with self.assertRaises(TypeError):
             f = MakeFrameNumeric(123)
-            
+
+    def testInitBadParams2(self):            
+        # Test bad calls to the constructor.
         with self.assertRaises(TypeError):
-            data = {'id':   [1],
-                    'value':[3]}
-                    
-            df = pd.DataFrame(data)
-            f = MakeFrameNumeric(df)
-            
+            f = MakeFrameNumeric([])
+ 
+    def testInitGood(self):
+        # Check we the constructor returns something sane if passed good params.
+        f = MakeFrameNumeric(self.validDataFrame)
+
+        self.assertIsInstance(f, MakeFrameNumeric)
+
+    
+    def testSetCardinalityForOneHot(self):
+        # Check we can override the maximum cardinality for one-hot encode.
+        f = MakeFrameNumeric(self.validDataFrame)
+        
+        self.assertNotEqual(f.maximum_cardinality_for_one_hot_encode, 123)
+        f.maximum_cardinality_for_one_hot_encode = 123
+        self.assertEqual(f.maximum_cardinality_for_one_hot_encode, 123)
+    
+
+    def testConvertAllNumeric(self):
+        # If we pass in an all-numeric dataframe, it should not be converted at all.
+        f = MakeFrameNumeric(self.validDataFrame)
+         
+        converted = f.ConvertForXGBoost()
+         
+        self.assertIsInstance(converted, pd.DataFrame)
+        self.assertTrue(converted.equals(self.validDataFrame))
+        
+ 
+    def testConvertSomeString(self):
+        # If we start with a string column with low cardinality, it should get converted to a one-hot column.
+        data = {'id':   [1,2,3],
+                'value':['A', 'B', 'C']}
+        df = pd.DataFrame(data)
+        f = MakeFrameNumeric(df)
+
+        f.maximum_cardinality_for_one_hot_encode = 3         
+        converted = f.ConvertForXGBoost()
+         
+        self.assertIsInstance(converted, pd.DataFrame)
+        
+        self.assertEqual(converted.shape, (3,4))
+        
+        self.assertIsInstance(converted.iloc[0,0], (np.int64, np.float64))        # id
+        self.assertIsInstance(converted.iloc[0,1], (np.int64, np.float64))        # 2nd column
+        self.assertIsInstance(converted.iloc[0,2], (np.int64, np.float64))        # 2nd column
+
+        # If we set the maximum cardinality lower, we should instead get a single converted column.        
+        f.maximum_cardinality_for_one_hot_encode = 2
+        converted = f.ConvertForXGBoost()         
+        self.assertIsInstance(converted, pd.DataFrame)
+        self.assertEqual(converted.shape, (3,2))
+        
+        
+if __name__ == '__main__':
+    unittest.main()
+    
