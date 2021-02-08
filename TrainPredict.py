f780a50866072191b2818d8fee3ac28eee3bbfc7
Will Smith <will@willsmith.org>
2021-02-08 00:10:38 +0000

    Separate out SpotErrors from Train and Predict

--- TrainPredict.py
+++ TrainPredict.py
@@ -22,15 +22,17 @@ class TrainPredict:
         self.__models_for_confidence = 10
         self.__zscore_for_error = 5
         
+        # Precreate a large number of xgboost models : 
+        #  -  firstly we create multiple models so we can push slightly different data at them to get a sense of the 
+        #    confidence of prediction by looking at the different results.
+        #  - secondly, we need a separate model to predict each column.
+        
         self.__models = [[XGBClassifier(verbosity=0) for j in range(self.__models_for_confidence)] for i in range(self.__numcols)]
 
  
     def Train(self):
-        # Eventually we need to train for each row of the source dataframe.   For now, we're only going to train on the 3nd column.
-        
-        
-        print(str(type(self.__models)))
-        
+        # Train multiple times on different subsets of the data to help us get a confidence interval.
+                
         for modelconf in range(self.__models_for_confidence):
 
             # We create one model for every column.
@@ -47,8 +49,10 @@ class TrainPredict:
                 self.__models[col][modelconf].fit(xtrain, ytrain)
         
     def Predict(self):
-        
 
+        self.__predictedmeans = np.zeros((self.__numrows, self.__numcols))
+        self.__predictedstds  = np.zeros((self.__numrows, self.__numcols))
+        
         # We create one model prediction for every column.
         for col in range(self.__numcols):  
             
@@ -60,12 +64,23 @@ class TrainPredict:
             ytest = np.zeros((self.__numrows, self.__models_for_confidence))
 
             # Get multiple predictions back on subtly different training data to give us a variation of results and a confidence interval.
+            # We don't accumulate predictions for the entire grid multiple times, because it might take a lot of memory to store.
             for modelconf in range(self.__models_for_confidence):                
                 ytest[:,modelconf] = self.__models[col][modelconf].predict(xtest)                
-                
+            
+            # Accumulate means and standard deviations of the predictions per column.  Then we throw the detailed prediction data away.
+            self.__predictedmeans[:,col] = np.mean(ytest, axis=1)
+            self.__predictedstds[:,col]  = np.std(ytest, axis=1)
+            
+        return(self.__predictedmeans, self.__predictedstds)       
+            
+    
+    def SpotErrors(self):
+        for col in range(self.__numcols):             
             for row in range(self.__numrows):
-                cellmean = np.mean(ytest[row,:])
-                cellstd  = np.std(ytest[row,:])
+
+                cellmean = self.__predictedmeans[row,col]
+                cellstd  = self.__predictedstds[row,col]
                 
                 if cellstd > 0:
                     zscore = np.abs(cellmean - self.__numpydf[row,col]) / cellstd
@@ -82,6 +97,5 @@ class TrainPredict:
                     diffmsg = ''
                     
                 print('['+str(row)+','+str(col)+'] actual:' + str(self.__numpydf[row,col]) + ' prediction:' + str(cellmean) + ' (stdev '+str(cellstd) + ')' + diffmsg)
-                
             
         
