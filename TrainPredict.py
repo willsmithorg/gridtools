212dd92b4361c928ef249084e652278a2bc333c6
Will Smith <will@willsmith.org>
2021-02-08 23:13:00 +0000

    GRD-23 added unit tests to TestPredict.  It now outputs a boolean array of Bad detections

--- TrainPredict.py
+++ TrainPredict.py
@@ -10,6 +10,9 @@ class TrainPredict:
 
     def __init__(self, df):
          
+        if (isinstance(df, dict)):
+            df = pd.DataFrame(df)
+        
         if( not isinstance(df, pd.DataFrame)):
             raise(TypeError,'df must be a DataFrame not a ' + str(type(df)))
 
@@ -22,17 +25,24 @@ class TrainPredict:
         self.__models_for_confidence = 10
         self.__zscore_for_error = 5
         
+
+
+        self.__models = None
+        self.__predictedmeans = None
+        self.__predictedstds = None
+        self.__boolerrors = None
+        
+ 
+    def Train(self):
+        
         # Precreate a large number of xgboost models : 
         #  -  firstly we create multiple models so we can push slightly different data at them to get a sense of the 
         #    confidence of prediction by looking at the different results.
         #  - secondly, we need a separate model to predict each column.
         
-        self.__models = [[XGBClassifier(verbosity=0) for j in range(self.__models_for_confidence)] for i in range(self.__numcols)]
-
+        self.__models = [[XGBClassifier(verbosity=0, nthread=4) for j in range(self.__models_for_confidence)] for i in range(self.__numcols)]
  
-    def Train(self):
-        # Train multiple times on different subsets of the data to help us get a confidence interval.
-                
+        # Train multiple times on different subsets of the data to help us get a confidence interval. 
         for modelconf in range(self.__models_for_confidence):
 
             # We create one model for every column.
@@ -47,9 +57,14 @@ class TrainPredict:
                 xtrain, xtest, ytrain, ytest = model_selection.train_test_split(xtrain, ytrain, train_size=0.7)
                 
                 self.__models[col][modelconf].fit(xtrain, ytrain)
+
         
     def Predict(self):
 
+        # Train, if we haven't already.
+        if self.__models is None:
+            self.Train()
+        
         self.__predictedmeans = np.zeros((self.__numrows, self.__numcols))
         self.__predictedstds  = np.zeros((self.__numrows, self.__numcols))
         
@@ -76,6 +91,12 @@ class TrainPredict:
             
     
     def SpotErrors(self):
+    
+        # Predict, if we haven't already.
+        if self.__predictedmeans is None:
+            self.Predict()
+    
+        self.__boolerrors = np.full((self.__numrows, self.__numcols), False)
         for col in range(self.__numcols):             
             for row in range(self.__numrows):
 
@@ -90,12 +111,29 @@ class TrainPredict:
                     else:
                         zscore = 0
                         
-                
+                # If bad, flag it as bad
                 if zscore >= self.__zscore_for_error:
+                    self.__boolerrors[row, col] = True
+                    
+        return self.__boolerrors
+        
+        
+    def PrintErrors(self):
+        # Spot errors, if we haven't already.
+        if self.__boolerrors is None:
+            self.SpotErrors()
+        
+        for col in range(self.__numcols):             
+            for row in range(self.__numrows):
+                
+                if self.__boolerrors[row,col]:
                     diffmsg = '  <---- DIFF' 
                 else: 
                     diffmsg = ''
                     
+                cellmean = self.__predictedmeans[row,col]
+                cellstd  = self.__predictedstds[row,col]
+                
                 print('['+str(row)+','+str(col)+'] actual:' + str(self.__numpydf[row,col]) + ' prediction:' + str(cellmean) + ' (stdev '+str(cellstd) + ')' + diffmsg)
             
         
