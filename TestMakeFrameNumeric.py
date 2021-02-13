51a610e7b2d469a150d9e97fa32afc7f9bc35640
Will Smith <will@willsmith.org>
2021-02-13 23:54:38 +0000

    All tests now pass again

--- TestMakeFrameNumeric.py
+++ TestMakeFrameNumeric.py
@@ -25,14 +25,14 @@ class TestMakeFrameNumeric(unittest.TestCase):
  
     def testInitGood(self):
         # Check we the constructor returns something sane if passed good params.
-        f = MakeFrameNumeric(self.validDataFrame)
+        f = MakeFrameNumeric()
 
         self.assertIsInstance(f, MakeFrameNumeric)
 
     
     def testSetCardinalityForOneHot(self):
         # Check we can override the maximum cardinality for one-hot encode.
-        f = MakeFrameNumeric(self.validDataFrame)
+        f = MakeFrameNumeric()
         
         self.assertNotEqual(f.maximum_cardinality_for_one_hot_encode, 123)
         f.maximum_cardinality_for_one_hot_encode = 123
@@ -41,9 +41,9 @@ class TestMakeFrameNumeric(unittest.TestCase):
 
     def testConvertAllNumeric(self):
         # If we pass in an all-numeric dataframe, it should not be converted at all.
-        f = MakeFrameNumeric(self.validDataFrame)
+        f = MakeFrameNumeric()
          
-        (converted, colmapd2s, colmaps2d, featuremapd, featuremaps, coltyped, coltypes) = f.Convert()
+        converted = f.Convert(self.validDataFrame)
          
         self.assertIsInstance(converted, pd.DataFrame)
         self.assertTrue(converted.equals(self.validDataFrame))
@@ -54,10 +54,10 @@ class TestMakeFrameNumeric(unittest.TestCase):
         data = {'id':   [1,2,3],
                 'value':['A', 'B', 'C']}
         df = pd.DataFrame(data)
-        f = MakeFrameNumeric(df)
+        f = MakeFrameNumeric()
 
         f.maximum_cardinality_for_one_hot_encode = 3         
-        (converted, colmapd2s, colmaps2d, featuremapd, featuremaps, coltyped, coltypes) = f.Convert()
+        converted = f.Convert(df)
          
         self.assertIsInstance(converted, pd.DataFrame)
         
@@ -70,7 +70,7 @@ class TestMakeFrameNumeric(unittest.TestCase):
         
         # If we set the maximum cardinality lower than the column's cardinality, we should instead get a single converted column.        
         f.maximum_cardinality_for_one_hot_encode = 2
-        (converted, colmapd2s, colmaps2d, featuremapd, featuremaps, coltyped, coltypes)  = f.Convert()         
+        converted  = f.Convert(df)         
         self.assertIsInstance(converted, pd.DataFrame)
         self.assertEqual(converted.shape, (3,2))
         
@@ -85,9 +85,9 @@ class TestMakeFrameNumeric(unittest.TestCase):
             dict['onehot'+str(col)] = [ 'A','A','A','A','A','B','B','B','B','B']
 
         df = pd.DataFrame(dict)
-        f = MakeFrameNumeric(df)        
+        f = MakeFrameNumeric()        
         f.maximum_cardinality_for_one_hot_encode = 3         
-        (converted, colmapd2s, colmaps2d, featuremapd, featuremaps, coltyped, coltypes) = f.Convert()
+        converted = f.Convert(df)
         self.assertIsInstance(converted, pd.DataFrame)
         self.assertEqual(converted.shape, (10,1+20+(2*20)))  # 1 for the id, 20 for the high-cardinality column, and 2 for each of 20 one-hots, for the low cardinality columns
          
@@ -102,38 +102,38 @@ class TestMakeFrameNumeric(unittest.TestCase):
                  'manyvalues':['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']}
             
         frame = pd.DataFrame(data)
-        mfn = MakeFrameNumeric(frame)
+        mfn = MakeFrameNumeric()
         mfn.maximum_cardinality_for_one_hot_encode = 10   # Hardcode so we don't get unexpected behaviour below if we change the default in the class.
 
-        (converted, colmapd2s, colmaps2d, featuremapd, featuremaps, coltyped, coltypes) = mfn.Convert()
+        converted = mfn.Convert(frame)
         
 
         # The colmapd2s is a mapping from destination to source column.
-        self.assertEqual(colmapd2s, 
+        self.assertEqual(mfn.colmapd2s, 
             {'country_0': 'country', 'country_1': 'country', 'country_2': 'country', 
             'currency_0': 'currency', 'currency_1': 'currency', 'currency_2': 'currency', 'currency_3': 'currency', 
             'manyvalues': 'manyvalues'})
             
         # The colmaps2d is a mapping from source to destinationcolumn.
-        self.assertEqual(colmaps2d, 
+        self.assertEqual(mfn.colmaps2d, 
             {'country': ['country_0', 'country_1', 'country_2' ], 
              'currency': ['currency_0', 'currency_1', 'currency_2', 'currency_3' ], 
              'manyvalues': 'manyvalues'})            
             
         # The featuremap is a mapping from converted value to source token, per destination column.
-        self.assertEqual(featuremapd, 
+        self.assertEqual(mfn.featuremapd, 
             {'country_0': 'Germany', 'country_1': 'UK', 'country_2': 'US', 
              'currency_0': 'EUR', 'currency_1': 'GBP', 'currency_2': 'MXN', 'currency_3': 'USD', 
              'manyvalues': {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 
                             9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O'}})
 
-        self.assertCountEqual(featuremaps['country'], ['Germany', 'UK', 'US'])
-        self.assertCountEqual(featuremaps['currency'], ['EUR', 'GBP', 'MXN', 'USD']);
+        self.assertCountEqual(mfn.featuremaps['country'], ['Germany', 'UK', 'US'])
+        self.assertCountEqual(mfn.featuremaps['currency'], ['EUR', 'GBP', 'MXN', 'USD']);
         
-        self.assertEqual(featuremaps['manyvalues'],  {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 
+        self.assertEqual(mfn.featuremaps['manyvalues'],  {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 
                             9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O'})
                             
-        self.assertEqual(coltyped, 
+        self.assertEqual(mfn.coltyped, 
             {'country_0': 'onehot', 'country_1': 'onehot', 'country_2': 'onehot', 
              'currency_0': 'onehot', 'currency_1': 'onehot', 'currency_2': 'onehot', 'currency_3': 'onehot', 
              'manyvalues': 'labelencoded'})
