3a6a302cdb4f72c2a7e87096e6b441e5dd788990
Will Smith <will@willsmith.org>
2021-02-16 21:37:08 +0000

    quick batch file to run all tests

---
+++ runtests.bat
@@ -0,0 +1,8 @@
+REM Embarrassing that this is a Windows batch file not sensible Linux shell script
+REM but for some reason my git bash doesn't run python.
+REM So this is a quick workaround.
+REM
+
+@echo off
+python TestMakeFrameNumeric.py
+python TestTrainPredict.py
