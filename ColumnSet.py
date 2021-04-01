f0e1214231a22d142717dc2b564fd3a09e3b7dfe
Will Smith <will@willsmith.org>
2021-04-02 00:53:03 +0100

    Starting to add ColumnSet to load and process an entire dataframe

---
+++ ColumnSet.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import scipy as scipy
+import numpy as np
+from AddDerivedColumns import AddDerivedColumns
+from Column import Column
+
+from cached_property import cached_property
+import logging
+logging.basicConfig(level=logging.INFO, datefmt='%H:%M:%S', format='%(asctime)s.%(msecs)03d - %(filename)s:%(lineno)d - %(message)s')
+
+
+class ColumnSet:
+
+    inputcolumns = []
+    derivedcolumns = []
+    
+    deriversToUse = ['Abs', 
+                     'Date', 
+                     'Len', 
+                     'MinMaxScaler', 
+                     'RobustScaler',
+                     'SentenceEmbedder', 
+                     'TokenizerCharDecimal', 
+                     'Upper' ]
+    
+    
+    def __init__(self, df):
+    
+        if not isinstance(df, pd.DataFrame):
+            raise(TypeError,'df must be a DataFrame not a ' + str(type(df)))
+            
+            
+        for col in df:
+            series = df[col]
+            #print(type(series))
+            self.inputcolumns.append(Column(series))
+            
+        #print(self.inputcolumns)
+    
+    
+    
+    def AddDerived(self):
+    
+        # Create the AddDerivedColumns processor and register all the derivers.
+        adc = AddDerivedColumns()        
+        for deriver in self.deriversToUse:
+            adc.Register(deriver)
+                         
+        for col in self.inputcolumns: 
+            print(col.name)
+            newcols = adc.Process(col)
+            self.derivedcolumns.extend(newcols)
+        
+        for col in self.derivedcolumns:
+            print('col:', col.name, ' parent:' , col.parent.name if col.parent is not None else '-')
+            print(col)
+
---
+++ GetVersions.py
@@ -0,0 +1,8 @@
+import scipy
+print('scipy : %s' % scipy.__version__)
+import numpy
+print('numpy : %s' % numpy.__version__)
+import tensorflow
+print('tensorflow : %s' % tensorflow.__version__)
+import keras
+print('keras: %s' % keras.__version__)
