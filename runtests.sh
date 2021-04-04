47c68c64b91c2d42eba9fbce15824020e437e627
Will Smith <will@willsmith.org>
2021-04-04 22:04:41 +0100

    Better layout of unittests and test discovery

--- runtests.sh
+++ runtests.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-py TestColumn.py
-
-py TestAddDerivedColumns.py
-py TestColumnDeriverAbs.py
-py TestColumnDeriverDate.py
-py TestColumnDeriverLen.py
-py TestColumnDeriverMinMaxScaler.py
-py TestColumnDeriverRobustScaler.py
-py TestColumnDeriverTokenizerCharDecimal.py
-py TestColumnDeriverUpper.py
-# py  -W ignore TestColumnDeriverSentenceEmbedder.py
-
-py TestMakeNumericColumns.py
-py TestColumnNumericerLabelEncoded.py
-
-py TestColumnSet.py
+python -m unittest $1 $2 $3
